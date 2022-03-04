ba06b62ed8048fa739bc180927be35d2e8fdb28f
PolarisNorthFor <30953700+zhongquan1234@users.noreply.github.com>
2022-03-04 15:04:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,6 @@
 # echo 'src-git openwrt-sysuh3c https://github.com/LGA1150/openwrt-sysuh3c' >>feeds.conf.default
 # echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git clone https://github.com/LGA1150/openwrt-sysuh3c.git package/openwrt-sysuh3c
