30ee712325e3ba9b90ebef02229aaa20d585b073
PolarisNorthFor <30953700+zhongquan1234@users.noreply.github.com>
2022-03-04 11:30:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,7 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-'src-git openwrt-sysuh3c https://github.com/LGA1150/openwrt-sysuh3c' >>feeds.conf.default
+echo 'src-git openwrt-sysuh3c https://github.com/LGA1150/openwrt-sysuh3c' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+
